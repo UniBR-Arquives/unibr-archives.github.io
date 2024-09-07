11bb8bd57882ebc0e4b91e823de034e81b5fa035
TheMrWest <mister.west529@gmail.com>
2024-09-06 21:26:07 -0300

    update-devlopment-v5

--- src/dist/script.ts
+++ src/dist/script.ts
@@ -1,7 +1,9 @@
 class Main {
     mainNameSite: string | undefined;
-    constructor(srcName: string | undefined = undefined) {
+    nameGame: string | undefined;
+    constructor(srcName: string | undefined = undefined, nameGame: string | undefined = undefined) {
         this.mainNameSite = srcName;
+        this.nameGame = nameGame;
     }
 
     setup(data) {
@@ -10,18 +12,93 @@ class Main {
         this.setupSiteName(data, 'name')
 
         this.returnName()
+        this.returnNameGame()
+
+        this.setupAbout(data, 'abouts-src');
     }
 
+    returnNameGame() {
+        let main = document.querySelectorAll(".nameGame")
+        main.forEach(data => {
+            console.log(data)
+            data.textContent = this.nameGame;
+        })
+    }
     returnName() {
         let main = document.querySelectorAll(".nameMain")
         main.forEach(data => {
             data.textContent = this.mainNameSite;
         })
     }
+    
+    setupAbout(data, nameId: string) {
+        if(Array.isArray(data['abouts'])) {
+            let abouts_src = document.getElementById(nameId)
+            if(abouts_src) {
+                abouts_src.innerHTML = '';
+                data['abouts'].forEach(function (element) {
+                    if(element.title && element.img && element.description && element.video && element.align && element.animation) {
+                        const divMain = document.createElement("div");
 
+                        const boxAboutDiv = document.createElement('div');
+                        boxAboutDiv.classList.add('box-about');
+
+                        const imgElement = document.createElement('img');
+                        imgElement.src = element.img;
+                        imgElement.classList.add('orie-img')
+                        
+                        const h3Element = document.createElement('h3');
+                        h3Element.textContent = element.title;
+
+                        const pElement = document.createElement('p');
+                        pElement.textContent = element.description;
+
+                        const externalVideo = document.createElement('img')
+                        externalVideo.src = element.video;
+                        externalVideo.classList.add("imgAbout")
+
+                        const gifVideo = document.createElement('img')
+                        gifVideo.src = element.animation;
+                        gifVideo.classList.add("gifAbout")
+
+                        const left = function (): void {
+                            divMain.classList.add('flex-left');
+                            divMain.appendChild(boxAboutDiv)
+                            divMain.appendChild(externalVideo)
+                            divMain.appendChild(gifVideo)
+                        }
+
+                        const right = function (): void {
+                            divMain.classList.add('flex-right');
+                            divMain.appendChild(gifVideo)
+                            divMain.appendChild(externalVideo)
+                            divMain.appendChild(boxAboutDiv)
+                        }
+
+                        boxAboutDiv.appendChild(imgElement);
+                        boxAboutDiv.appendChild(h3Element)
+                        boxAboutDiv.appendChild(pElement)
+
+                        if(element.align == "left") {
+                            left();
+                        } else if (element.align == "right") {
+                            right();
+                        } else {
+                            left();
+                        }
+
+                        abouts_src.appendChild(divMain);
+
+                    }
+                })
+            }
+        }
+
+    }
     setupSiteName(data, nameId: string) {
         let main = document.getElementById(nameId)
         main.textContent = data['main'].title;
+        this.nameGame = data['main'].game;
     }
 
     setupSiteHeader(data, navId: string) {
@@ -42,7 +119,6 @@ class Main {
             }
         }
     }
-
 }
 
 function setup() {
--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
 var Main = /** @class */ (function () {
-    function Main(srcName) {
+    function Main(srcName, nameGame) {
         if (srcName === void 0) { srcName = undefined; }
+        if (nameGame === void 0) { nameGame = undefined; }
         this.mainNameSite = srcName;
+        this.nameGame = nameGame;
     }
     Main.prototype.setup = function (data) {
         if (this.mainNameSite == undefined)
@@ -9,6 +11,16 @@ var Main = /** @class */ (function () {
         this.setupSiteHeader(data, 'nav');
         this.setupSiteName(data, 'name');
         this.returnName();
+        this.returnNameGame();
+        this.setupAbout(data, 'abouts-src');
+    };
+    Main.prototype.returnNameGame = function () {
+        var _this = this;
+        var main = document.querySelectorAll(".nameGame");
+        main.forEach(function (data) {
+            console.log(data);
+            data.textContent = _this.nameGame;
+        });
     };
     Main.prototype.returnName = function () {
         var _this = this;
@@ -17,9 +29,63 @@ var Main = /** @class */ (function () {
             data.textContent = _this.mainNameSite;
         });
     };
+    Main.prototype.setupAbout = function (data, nameId) {
+        if (Array.isArray(data['abouts'])) {
+            var abouts_src_1 = document.getElementById(nameId);
+            if (abouts_src_1) {
+                abouts_src_1.innerHTML = '';
+                data['abouts'].forEach(function (element) {
+                    if (element.title && element.img && element.description && element.video && element.align && element.animation) {
+                        var divMain_1 = document.createElement("div");
+                        var boxAboutDiv_1 = document.createElement('div');
+                        boxAboutDiv_1.classList.add('box-about');
+                        var imgElement = document.createElement('img');
+                        imgElement.src = element.img;
+                        imgElement.classList.add('orie-img');
+                        var h3Element = document.createElement('h3');
+                        h3Element.textContent = element.title;
+                        var pElement = document.createElement('p');
+                        pElement.textContent = element.description;
+                        var externalVideo_1 = document.createElement('img');
+                        externalVideo_1.src = element.video;
+                        externalVideo_1.classList.add("imgAbout");
+                        var gifVideo_1 = document.createElement('img');
+                        gifVideo_1.src = element.animation;
+                        gifVideo_1.classList.add("gifAbout");
+                        var left = function () {
+                            divMain_1.classList.add('flex-left');
+                            divMain_1.appendChild(boxAboutDiv_1);
+                            divMain_1.appendChild(externalVideo_1);
+                            divMain_1.appendChild(gifVideo_1);
+                        };
+                        var right = function () {
+                            divMain_1.classList.add('flex-right');
+                            divMain_1.appendChild(gifVideo_1);
+                            divMain_1.appendChild(externalVideo_1);
+                            divMain_1.appendChild(boxAboutDiv_1);
+                        };
+                        boxAboutDiv_1.appendChild(imgElement);
+                        boxAboutDiv_1.appendChild(h3Element);
+                        boxAboutDiv_1.appendChild(pElement);
+                        if (element.align == "left") {
+                            left();
+                        }
+                        else if (element.align == "right") {
+                            right();
+                        }
+                        else {
+                            left();
+                        }
+                        abouts_src_1.appendChild(divMain_1);
+                    }
+                });
+            }
+        }
+    };
     Main.prototype.setupSiteName = function (data, nameId) {
         var main = document.getElementById(nameId);
         main.textContent = data['main'].title;
+        this.nameGame = data['main'].game;
     };
     Main.prototype.setupSiteHeader = function (data, navId) {
         if (Array.isArray(data['navigation'])) {
